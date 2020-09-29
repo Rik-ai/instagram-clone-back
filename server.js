81fc46270580c7a93fd07e758f1c75c59a6461ab
Ivan <ivan.lakhanin@gmail.com>
2020-09-29 15:54:44 +0300

    First API & test

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
-import Pusher from 'pusher'
-
+const express = require('express')
+const cors = require ('cors')
+const mongoose = require ('mongoose')
+const Pusher = require ('pusher')
 
 //app config
+const app = express()
+const port = process.env.PORT || 8080
 
 
 //middlewares
+app.use(express.json())
+app.use(cors())
 
 
 //DB config
 
 
 //api routes
-
+app.get('/', (req, res) => res.status(200).send('hello world'))
 
 //listener
+app.listen(port, () => console.log(`listening on localhost:${port}`))
